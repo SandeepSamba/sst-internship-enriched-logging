98f9910e390258391c0225bdc9157b18b74bf005
Jelwin13afc <jelwin13afc@gmail.com>
2020-05-28 14:32:00 +0530

    Initial Commit for .NET application

---
+++ Sample_.NET/Sample_.NET/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace nlogexampe
+{
+    class Program
+    {
+        private static Logger Mysameplelogger =
+         LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            LogSample();
+        }
+
+        static void LogSample()
+        {
+
+            Mysameplelogger.Info("ss");
+
+
+        }
+    }
+}
