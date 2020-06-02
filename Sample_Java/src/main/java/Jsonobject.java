a184245951ef41999740f6d3197e2f242f967d58
valkyr13 <vandana.swj@gmail.com>
2020-06-02 11:09:34 +0530

    Update Jsonobject.java

--- Sample_Java/src/main/java/Jsonobject.java
+++ Sample_Java/src/main/java/Jsonobject.java
@@ -7,15 +7,13 @@ public class Jsonobject {
 	    String callingclassname;
 	    ArrayList<attributeclass> attributes;
 	    ArrayList<attributetypes> attributetype;
-	    ArrayList<Object> temp;
 	    
-	    public Jsonobject(String timestamp,String classname,String methodname,String callingclassname,ArrayList<Object> attributes,ArrayList<String> attributetype) {
+	    public Jsonobject(String timestamp,String classname,String methodname,String callingclassname,ArrayList<Object> attributes,ArrayList<String> attributetype,temp) {
 	    	this.timestamp = timestamp;
 	    	this.callingclassname = callingclassname;
 	    	this.classname = classname;
 	    	this.methodname = methodname;
-	    	temp = attributes;
-	    	for(int i = 0;i<temp.size();i++) {
+	    	for(int i = 0;i<temp;i++) {
 	    		this.attributes.add(new attributeclass(attributes.get(i)));
 		    	this.attributetype.add(new attributetypes(attributetype.get(i)));
 	    	} 
