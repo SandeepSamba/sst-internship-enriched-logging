fcd24a8f559f58408485ccab0ff02d1851b003dc
valkyr13 <vandana.swj@gmail.com>
2020-06-02 11:10:36 +0530

    Update Jsonconvert.java

--- Sample_Java/src/main/java/Jsonconvert.java
+++ Sample_Java/src/main/java/Jsonconvert.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 public class Jsonconvert {
 
 	
-	public String convert(String timestamp,String classname,String methodname,String callingclassname,ArrayList<Object> attributes,ArrayList<String> attributetype) {
+	public String convert(String timestamp,String classname,String methodname,String callingclassname,ArrayList<Object> attributes,ArrayList<String> attributetype, temp) {
 		
 		Jsonobject jsonobj = new Jsonobject(timestamp,classname, methodname,
-				callingclassname,attributes,attributetype);
+				callingclassname,attributes,attributetype,temp);
 		Gson gson = new Gson();
 		String Json = gson.toJson(jsonobj);  
 		
